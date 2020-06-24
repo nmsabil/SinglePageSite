d6dd8e6389ab05b80be8ae5c59a02c3c4d55853b
Sabil <sabilmdtv1@gmail.com>
2020-06-24 16:52:11 +0100

    named the functions and classes to more appropriate names

--- js/main.js
+++ js/main.js
@@ -1,41 +1,45 @@
 // navbar dark on scroll
-window.onscroll = function() {
+window.onscroll = function () {
     navBarSticky();
 };
 
-var navbar = document.getElementById("navbar");
 
-var sticky = navbar.offsetTop;
+let topPosition = navbar.offsetTop;
+
 // navbar fixed on scroll
 function navBarSticky() {
-    if (window.pageYOffset >= sticky) {
-        navbar.style.position = "fixed";
-        navbar.style.top = "0";
-        navbar.style.background = "rgba(51,51,51,1.0)";
+    const navbar = document.getElementById("navbar");
+    let navbarStyle = navbar.style;
+    if (window.pageYOffset >= topPosition) {
+        navbarStyle.position = "fixed";
+        navbarStyle.top = "0";
+        navbarStyle.background = "rgba(51,51,51,1.0)";
     } else {
-        navbar.style.position = "absolute";
-        navbar.style.top = "30px";
-        navbar.style.background = "rgba(51,51,51,0.6)";
+        navbarStyle.position = "absolute";
+        navbarStyle.top = "30px";
+        navbarStyle.background = "rgba(51,51,51,0.6)";
     }
 }
 
-// youtube video popup
-$(".bla-1").modalVideo();
 
-// Number counter up
-$(".count_number").counterUp({
+
+// youtube video popup plugin
+$(".youtubeVideo").modalVideo();
+
+//Stats counter up plugin
+$(".animationCountUp").counterUp({
     delay: 10,
     time: 1000,
 });
 
-// gallery
-$("#work").imagesLoaded(function() {
+// Recent Work Gallery 
+$("#work").imagesLoaded(function () {
     $(".galary_item").isotope({
         itemSelector: ".singal_item",
         layoutMode: "fitRows",
     });
-    // Add isotope click function
-    $(".portfolio-filter li").on("click", function() {
+    // Filter work -Isotope plugin
+    $(".portfolio-filter li").on("click", function () {
         $(".portfolio-filter li").removeClass("active");
         $(this).addClass("active");
 
@@ -52,8 +56,7 @@ $("#work").imagesLoaded(function() {
     });
 });
 
-// testnomial carousel
-
+// testnomial carousel - OwlCarousel Plugin
 $(".testmonial_active").owlCarousel({
     loop: true,
     margin: 20,
@@ -73,8 +76,7 @@ $(".testmonial_active").owlCarousel({
     },
 });
 
-// brand slide active
-
+// brand slide autoplay
 $(".brand_slide_active").owlCarousel({
     loop: true,
     margin: 20,
@@ -94,50 +96,46 @@ $(".brand_slide_active").owlCarousel({
     },
 });
 
-//scroll to top active
-
-$("#scroll_top").on("click", function() {
+//button to scroll the the top
+$("#scroll_top").on("click", function () {
     $("html,body").animate({
-            scrollTop: 0,
-        },
+        scrollTop: 0,
+    },
         2000
     );
 });
 
-// scroll spy active
-
+// bootstrap spy scroll
 $("body").scrollspy({ target: "#navbar", offset: 50 });
-$("#scroll_spy_nav li a").on("click", function(event) {
+$("#scroll_spy_nav li a").on("click", function (event) {
     if (this.hash !== "") {
         event.preventDefault();
         var hash = this.hash;
         $("html, body").animate({
-                scrollTop: $(hash).offset().top,
-            },
+            scrollTop: $(hash).offset().top,
+        },
             890,
-            function() {
+            function () {
                 window.location.hash = hash;
             }
         );
     }
 });
 
-// add active class to nav links on scroll
-$(window).scroll(function() {
+// add active class to nav links when scrolled
+$(window).scroll(function () {
     $(".nav-item").removeClass("active");
     $(".active").parent().addClass("active");
 });
 
-// hide navbar on click outside for phones
-
-$(function() {
-    $(document).click(function(event) {
+// hide collapsed navbar when clicked outside, for the small devices
+$(function () {
+    $(document).click(function (event) {
         $(".navbar-collapse").collapse("hide");
     });
 });
 
-// preloader animation out class
-
-$(window).on("load", function() {
+// adds "out" class to remove the pageload innimation
+$(window).on("load", function () {
     $(".loader").addClass("out");
 });
